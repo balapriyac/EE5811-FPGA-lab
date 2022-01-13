0e70d3478b36c59544b611a4253f73237e979032
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2022-01-13 15:35:41 +0530

    Add files for `Assignment-1`

---
+++ Assignment-1/EE5811_A1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    unsigned char  X=0x01,Y=0x00,Z=0x00;
+    unsigned char one = 0x01;
+    unsigned char F;
+    F = (~X) | Y;
+    printf("F = %x", one&F);
+	return 0;
+}
