c4929ba2f91eecc843daea5eaf6bbe766dad8a36
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2022-01-14 20:37:12 +0530

    Edit: To receive inputs X, Y, Z from user

--- Assignment-1/EE5811_A1[revised].c
+++ Assignment-1/EE5811_A1[revised].c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    unsigned char  X=0x01,Y=0x01,Z=0x00;
+    unsigned char  X,Y,Z;
+    printf("X: ");
+    scanf("%x", &X);
+    printf("Y: ");
+    scanf("%x", &Y);
+    printf("Z: ");
+    scanf("%x", &Z);
+    
     unsigned char one = 0x01;
     unsigned char F;
     F = ~(X&(~Y));
-    printf("F = %x", one&F);
+    printf("F = %x\n", one&F);
     return 0;
 }
+
