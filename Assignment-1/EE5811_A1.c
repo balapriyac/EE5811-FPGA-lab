daa03e26c729ba5a7485d24ca894b266dcb68968
Bala Priya C <47279635+balapriyac@users.noreply.github.com>
2022-01-15 19:07:05 +0530

    Add comments

--- Assignment-1/EE5811_A1.c
+++ Assignment-1/EE5811_A1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    unsigned char  X=0x00,Y=0x00,Z=0x00;
+    unsigned char  X=0x00,Y=0x00,Z=0x00; //input bits in hex representation
     unsigned char one = 0x01;
     unsigned char F;
     F = ~(X & (~Y));
-    printf("XYZ = %x%x%x : Output F = %x\n", one&X,one&Y,one&Z,one&F);
+    printf("XYZ = %x%x%x : Output F = %x\n", one&X,one&Y,one&Z,one&F); 
     return 0;
 }
 
